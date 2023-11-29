32671b68665c55651a610c569b3a6af17ad8ef0a
Kushagra Bankey <kushagrabankey1703@gmail.com>
2023-11-29 20:35:12 +0530

    initial commit v1

---
+++ src/Components/Tile.js
@@ -0,0 +1,12 @@
+export default function Tile({ className, value, onClick, turn }) {
+    let hoverClass = null;
+    if (value == null && turn != null) {
+      hoverClass = `${turn.toLowerCase()}-hover`;
+    }
+  
+    return (
+      <div onClick={onClick} className={`tile ${className} ${hoverClass}`}>
+        {value}
+      </div>
+    );
+  }
